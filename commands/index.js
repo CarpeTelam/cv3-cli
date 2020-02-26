c50c199d547359ec392fd72e3b85aae5d1ac1e16
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-02-26 22:33:27 +0000

    Add watch functionality to `yarn dev`

--- commands/index.js
+++ commands/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text } from "ink";
 
 import Hello from "../src/components/Hello";
 
 /// CV3 CLI tool for local development
 function CV3(props) {
-  return <Hello name={props.name} />;
+  return <Hello color={props.color} name={props.name} />;
 }
 
 CV3.propTypes = {
+  /// The "color" option can be used to alter the greeting's color
+  color: PropTypes.string,
   /// The "name" option can be used to alter the greeting
-  name: PropTypes.string.isRequired
+  name: PropTypes.string
 };
 
 CV3.defaultProps = {
+  color: "blue",
   name: "Stranger"
 };
 
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text } from "ink";
+import { Color, Text } from "ink";
 
 function Hello(props) {
-  return <Text>Hello, {props.name}</Text>;
+  const color = props.name === "Stranger" ? "orange" : props.color;
+  return (
+    <Text>
+      Hello, <Color keyword={color}>{props.name}</Color>
+    </Text>
+  );
 }
 
 Hello.propTypes = {
+  color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired
 };
 
