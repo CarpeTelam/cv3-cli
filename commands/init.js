d8aabc6cf2b4702369bcc33d46de3f493b1fe478
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-03-20 04:48:46 +0000

    Finialize init command

--- commands/init.js
+++ commands/init.js
@@ -1,89 +1,103 @@
 import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import fs from "fs";
+import moment from "moment";
 import { AppContext, Box, Color, Text } from "ink";
 
 import JsonContent from "../src/components/JsonContent";
 import TextInput from "../src/components/TextInput";
 
-const defaultInputs = JSON.parse(fs.readFileSync("./store.json"));
+function loadJSON(path) {
+  if (!fs.existsSync(path)) {
+    return {};
+  }
+  return JSON.parse(fs.readFileSync(path));
+}
+
+const defaultInputs = {
+  username: "",
+  password: "",
+  id: "",
+  stagingURL: "",
+  ...loadJSON("./cv3-credentials.json"),
+  ...loadJSON("./store.json")
+};
 
 /// Init CV3 store repo
 function init() {
-  if (fs.existsSync("./store.json")) {
-    const [inputs, setInputs] = useState(defaultInputs);
-    const [focus, setFocus] = useState(0);
-    const { exit } = useContext(AppContext);
+  const [inputs, setInputs] = useState(defaultInputs);
+  const [focus, setFocus] = useState(0);
+  const { exit } = useContext(AppContext);
 
-    const fields = [
-      { name: "id", label: "Store ID:", type: "number" },
-      { name: "stagingURL", label: "Staging URL:", type: "text" }
-    ];
+  const fields = [
+    { name: "username", label: "CV3 Username:", type: "text" },
+    { name: "password", label: "CV3 Password:", type: "password" },
+    { name: "id", label: "Store ID:", type: "number" },
+    { name: "stagingURL", label: "Staging URL:", type: "text" }
+  ];
 
-    function handleInput(key, value) {
-      setInputs({ ...inputs, [key]: value });
+  function handleInput(input) {
+    setInputs({ ...inputs, ...input });
+  }
+
+  function handleEscape() {
+    exit();
+  }
+
+  function handleNext(input) {
+    handleInput(input);
+    if (focus < fields.length - 1) {
+      setFocus(focus + 1);
     }
+  }
 
-    function handleEscape() {
+  function handlePrevious(input) {
+    handleInput(input);
+    if (focus > 0) {
+      setFocus(focus - 1);
+    }
+  }
+
+  function handleReturn(input) {
+    handleNext(input);
+    if (focus === fields.length - 1) {
+      setFocus(fields.length);
+      const { username, password, id, stagingURL } = inputs;
+      const timestamp = moment().unix();
+      const cv3Credentials = { username, password };
+      const store = { id, stagingURL, timestamp };
+      fs.writeFileSync(
+        "./cv3-credentials.json",
+        JSON.stringify(cv3Credentials, null, 2)
+      );
+      fs.writeFileSync("./store.json", JSON.stringify(store, null, 2));
       exit();
     }
-
-    function handleNext(key, value) {
-      handleInput(key, value);
-      if (focus < fields.length - 1) {
-        setFocus(focus + 1);
-      }
-    }
-
-    function handlePrevious(key, value) {
-      handleInput(key, value);
-      if (focus > 0) {
-        setFocus(focus - 1);
-      }
-    }
-
-    function handleReturn(key, value) {
-      handleNext(key, value);
-      if (focus === fields.length - 1) {
-        setFocus(fields.length);
-        exit();
-      }
-    }
-
-    return (
-      <Box flexDirection="column">
-        <JsonContent json={inputs} label="store.json:" valueColor="red" />
-        {fields.map((field, index) => (
-          <TextInput
-            defaultValue={defaultInputs[field.name]}
-            focus={focus === index}
-            key={field.name}
-            label={field.label}
-            labelColor={focus === index ? "magenta" : "white"}
-            name={field.name}
-            onArrowDown={handleNext}
-            onArrowUp={handlePrevious}
-            onChange={handleInput}
-            onEscape={handleEscape}
-            onReturn={handleReturn}
-            onShiftTab={handlePrevious}
-            onTab={handleNext}
-            placeholder={defaultInputs[field.name]}
-            type={field.type}
-            value={String(inputs[field.name] || "")}
-          />
-        ))}
-      </Box>
-    );
-  } else {
-    return (
-      <Box>
-        <Text bold>
-          <Color keyword="red">File Doesn't Exist</Color>
-        </Text>
-      </Box>
-    );
   }
+
+  return (
+    <Box flexDirection="column">
+      {fields.map((field, index) => (
+        <TextInput
+          defaultValue={defaultInputs[field.name]}
+          focus={focus === index}
+          key={field.name}
+          label={field.label}
+          name={field.name}
+          onArrowDown={handleNext}
+          onArrowUp={handlePrevious}
+          onChange={handleInput}
+          onEscape={handleEscape}
+          onReturn={handleReturn}
+          onShiftTab={handlePrevious}
+          onTab={handleNext}
+          placeholder={defaultInputs[field.name]}
+          type={field.type}
+          value={String(inputs[field.name])}
+        />
+      ))}
+    </Box>
+  );
 }
 
 export default init;
