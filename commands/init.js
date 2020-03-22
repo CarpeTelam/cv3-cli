635fef2cbdac161ff5970b16e2402ecf43266f79
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-03-22 07:49:02 +0000

    Everything working except exit()

--- commands/init.js
+++ commands/init.js
@@ -30,6 +30,8 @@ const defaultInputs = {
 function init() {
   const [inputs, setInputs] = useState(defaultInputs);
   const [focus, setFocus] = useState(0);
+  const [cv3CredentialsText, setCv3CredentialsText] = useState("");
+  const [storeText, setStoreText] = useState("");
   const { exit } = useContext(AppContext);
 
   const fields = [
@@ -39,13 +41,13 @@ function init() {
     { name: "stagingURL", label: "Staging URL:", type: "text" }
   ];
 
-  function handleInput(input) {
-    setInputs({ ...inputs, ...input });
-  }
-
   function handleEscape() {
     setFocus(fields.length);
     exit();
+  }
+
+  function handleInput(input) {
+    setInputs({ ...inputs, ...input });
   }
 
   function handleNext(input) {
@@ -66,25 +68,75 @@ function init() {
     handleNext(input);
     if (focus === fields.length - 1) {
       setFocus(fields.length);
-      if (inputs.username !== "" && inputs.password !== "") {
-        writeFile(cv3CredentialsPath, {
-          username: inputs.username,
-          password: inputs.password
-        });
-      }
-      if (inputs.id !== "" || inputs.stagingURL !== "") {
-        writeFile(storePath, {
-          id: inputs.id,
-          stagingURL: inputs.stagingURL,
-          timestamp: moment().unix()
-        });
-      }
-      exit();
+      writeConfigs();
+      // exit();
     }
   }
 
-  function writeFile(path, content) {
-    fs.writeFileSync(path, JSON.stringify(content, null, 2));
+  async function writeConfigs() {
+    const { username, password, id, stagingURL } = inputs;
+    const timestamp = moment().unix();
+
+    const cv3Credentials = JSON.stringify({ username, password }, null, 2);
+    const store = JSON.stringify({ id, stagingURL, timestamp }, null, 2);
+
+    const cv3CredentialsPromise =
+      username !== "" && password !== ""
+        ? fs.promises.writeFile(cv3CredentialsPath, cv3Credentials)
+        : Promise.reject(new Error("username or password blank"));
+
+    const storePromise =
+      id !== "" || stagingURL !== ""
+        ? fs.promises.writeFile(storePath, store)
+        : Promise.reject(new Error("id and stagingURL blank"));
+
+    cv3CredentialsPromise
+      .then(() => {
+        setCv3CredentialsText(
+          <>
+            <Color blackBright>
+              {`${moment().format("YYYY-MM-D HH:mm:ss.SSS")} `}
+            </Color>
+            <Color keyword="green">{cv3CredentialsPath} updated</Color>
+          </>
+        );
+      })
+      .catch(error => {
+        setCv3CredentialsText(
+          <>
+            <Color blackBright>
+              {`${moment().format("YYYY-MM-D HH:mm:ss.SSS")} `}
+            </Color>
+            <Color keyword="yellow">
+              ${cv3CredentialsPath} not updated, `}
+            </Color>
+            <Color keyword="red">error: {error.message}</Color>
+          </>
+        );
+      });
+
+    storePromise
+      .then(() => {
+        setStoreText(
+          <>
+            <Color blackBright>
+              {`${moment().format("YYYY-MM-D HH:mm:ss.SSS")} `}
+            </Color>
+            <Color keyword="green">{storePath} updated</Color>
+          </>
+        );
+      })
+      .catch(error => {
+        setStoreText(
+          <>
+            <Color blackBright>
+              {`${moment().format("YYYY-MM-D HH:mm:ss.SSS")} `}
+            </Color>
+            <Color keyword="yellow">{`${storePath} not updated, `}</Color>
+            <Color keyword="red">error: {error.message}</Color>
+          </>
+        );
+      });
   }
 
   return (
@@ -108,6 +160,8 @@ function init() {
           value={String(inputs[field.name])}
         />
       ))}
+      {cv3CredentialsText !== "" && <Box>{cv3CredentialsText}</Box>}
+      {storeText !== "" && <Box>{storeText}</Box>}
     </Box>
   );
 }
